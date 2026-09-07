84081328704c0a97f662089ada984ae9571fb0ce
akshaykhedkar9797-rgb <akshaykhedkar9797@gmail.com>
2026-09-07 16:16:20 +0530

    Update and rename ssecond.js to ssecond.html

--- ssecond.js
+++
@@ -1,9 +0,0 @@
-// Store two numbers in variables
-let number1 = 10;
-let number2 = 20;
-
-// Add the two numbers
-let sum = number1 + number2;
-
-// Display the result
-console.log("The sum is: " + sum);
