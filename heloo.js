ec7ba22ef99341b5273fb255989951567cfa7b79
akshaykhedkar9797-rgb <akshaykhedkar9797@gmail.com>
2026-09-07 16:12:33 +0530

    Create heloo.js

---
+++ heloo.js
@@ -0,0 +1,9 @@
+// Store a number in a variable
+let number = 10;
+
+// Check whether the number is even or odd
+if (number % 2 === 0) {
+    console.log(number + " is an Even number.");
+} else {
+    console.log(number + " is an Odd number.");
+}
