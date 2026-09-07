83f43d1ce0d18df219423fada995951ed9bf6da0
akshaykhedkar9797-rgb <akshaykhedkar9797@gmail.com>
2026-09-07 16:14:45 +0530

    Create ssecond.js

---
+++ ssecond.js
@@ -0,0 +1,9 @@
+// Store two numbers in variables
+let number1 = 10;
+let number2 = 20;
+
+// Add the two numbers
+let sum = number1 + number2;
+
+// Display the result
+console.log("The sum is: " + sum);
